78324c794f0464e417ef5fbacf65f6b5be6d5779
ryan2445 <ryan@snapdesk.app>
2022-03-20 18:49:22 -0700

    Event posting / retrieval

--- api/Services/Events.py
+++ api/Services/Events.py
@@ -1,6 +1,8 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 import json
 import os
+import uuid
 
 if os.getenv('AWS_SAM_LOCAL'):
     dynamodb = boto3.resource('dynamodb', endpoint_url = 'http://dynamodb-local:8000').Table('system')
@@ -9,21 +11,37 @@ else:
 
 def eventsPost(event, context):
     body = json.loads(event['body'])
+    username = event['requestContext']['authorizer']['claims']['email']
+
+    required_keys = ['title', 'description']
+
+    if not all(body.get(key) for key in required_keys):
+        return {
+            'statusCode': 400,
+            'body': 'Validation Error: Missing required keys.'
+        }
+    
+    event_id = str(uuid.uuid4())
+    
+    dynamodb.put_item(
+        Item = {
+            'pk': 'EVENT',
+            'sk': username + '#' + event_id,
+            'title': body['title'],
+            'description': body['description'],
+            'uuid': event_id
+        }
+    )
 
     return {
         'statusCode': 200,
-        'body': json.dumps({ 'response': 'Success!' })
+        'body': 'Success!'
     }
 
 def eventsGet(event, context):
     username = event['requestContext']['authorizer']['claims']['email']
 
-    response = dynamodb.get_item(
-        Key = {
-            'pk': 'EVENT',
-            'sk': username
-        }
-    )
+    response = dynamodb.query(KeyConditionExpression = Key('pk').eq("EVENT") & Key('sk').begins_with(username))
 
     items = response.get('Items') or []
 
