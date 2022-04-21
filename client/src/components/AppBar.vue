a614f2562a6fae54ad1127c7a6e7e15bcf6d17a0
Ryan Hoffman <rhoffman@copia.io>
2022-04-21 11:19:06 -0700

    minimizable navigation drawer

--- client/src/components/AppBar.vue
+++ client/src/components/AppBar.vue
@@ -1,63 +1,70 @@
 <template>
-    <v-app-bar color="primary" class="white--text" app clipped-left flat>
-        <v-app-bar-nav-icon>
-            <v-icon color="white">mdi-menu</v-icon>
-        </v-app-bar-nav-icon>
-        <v-toolbar-title>{{ user }}</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-menu left offset-y :loading="loading">
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn icon v-bind="attrs" v-on="on">
-                    <v-icon color="white">mdi-dots-vertical</v-icon>
-                </v-btn>
-            </template>
-            <v-list class="pa-2">
-                <v-list-item v-for="(item, i) in items" :key="i"
-                    @click="handleClick(item.method)" color="primary">
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-menu>
-    </v-app-bar>
+  <v-app-bar color="primary" class="white--text" app clipped-left flat>
+    <v-app-bar-nav-icon
+      @click="$emit('min', !navDrawerIsMin), (navDrawerIsMin = !navDrawerIsMin)"
+    >
+      <v-icon color="white">mdi-menu</v-icon>
+    </v-app-bar-nav-icon>
+    <v-toolbar-title>{{ user }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-menu left offset-y :loading="loading">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on">
+          <v-icon color="white">mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+      <v-list class="pa-2">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          @click="handleClick(item.method)"
+          color="primary"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
-    data() {
-        return {
-            loading: false,
-            items: [
-                {
-                    title: 'Sign Out',
-                    icon: 'mdi-exit-to-app',
-                    method: 'signOut'
-                }
-            ]
-        }
-    },
-    computed: {
-        ...mapGetters(['user'])
-    },
-    methods: {
-        handleClick(method) {
-            return this[method]()
+  data() {
+    return {
+      loading: false,
+      navDrawerIsMin: false,
+      items: [
+        {
+          title: "Sign Out",
+          icon: "mdi-exit-to-app",
+          method: "signOut",
         },
-        async signOut() {
-            this.loading = true
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["user"]),
+  },
+  methods: {
+    handleClick(method) {
+      return this[method]();
+    },
+    async signOut() {
+      this.loading = true;
 
-            const cognito = await this.$store.dispatch('signOut', {
-                accessToken: localStorage.AccessToken
-            })
+      const cognito = await this.$store.dispatch("signOut", {
+        accessToken: localStorage.AccessToken,
+      });
 
-            this.loading = false
+      this.loading = false;
 
-            if (!cognito) return
+      if (!cognito) return;
 
-            this.$router.push('/signin')
-        }
-    }
-}
+      this.$router.push("/signin");
+    },
+  },
+};
 </script>
--- client/src/components/NavDrawer.vue
+++ client/src/components/NavDrawer.vue
@@ -1,34 +1,40 @@
 <template>
-    <v-navigation-drawer app clipped>
-        <v-list nav>
-            <v-list-item-group active-class="primary--text text--accent-4">
-                <v-list-item v-for="(link, i) in links" :key="i" link :to="link.route">
-                    <v-list-item-icon>
-                        <v-icon>{{ link.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>{{ link.title }}</v-list-item-title>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
-    </v-navigation-drawer>
+  <v-navigation-drawer app clipped :mini-variant="isMin">
+    <v-list nav>
+      <v-list-item-group active-class="primary--text text--accent-4">
+        <v-list-item v-for="(link, i) in links" :key="i" link :to="link.route">
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            links: [
-                {
-                    title: 'Home',
-                    icon: 'mdi-home',
-                    route: '/home'
-                },
-                {
-                    title: 'Settings',
-                    icon: 'mdi-cog',
-                    route: '/settings'
-                }
-            ]
-        }
-    }
-}
+  props: {
+    isMin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      links: [
+        {
+          title: "Home",
+          icon: "mdi-home",
+          route: "/home",
+        },
+        {
+          title: "Settings",
+          icon: "mdi-cog",
+          route: "/settings",
+        },
+      ],
+    };
+  },
+};
 </script>
